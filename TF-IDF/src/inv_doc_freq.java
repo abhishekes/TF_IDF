5097c6b34d409f94389c60adf8f40dd58ff353e9
abhishekes <abhishek.es@gmail.com>
2012-12-01 19:01:51 -0600

    Draft implementation of TF-IDF

---
+++ TF-IDF/src/inv_doc_freq.java
@@ -0,0 +1,54 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+
+public class inv_doc_freq {
+    //Method to invoke : java inv_doc_freq <input_filename> <output_file_name>
+	public static void main(String[] args) {
+
+		BufferedReader br=null;
+		try {
+			br = new BufferedReader(new FileReader(args[0]));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("The specified file " + args[0] + "was not found");
+			e1.printStackTrace();
+		}
+        String line = "";
+        //HashMap<String, Integer> wordmap = new HashMap<String, Integer>();
+		Integer totalCount = 0;
+        try {
+			while ((line = br.readLine()) != null) {
+			    totalCount++;
+			}
+            br.close();
+            br = new BufferedReader(new FileReader(args[0]));
+            Double multiplier = Math.log (1/totalCount.doubleValue());
+            FileWriter fstream = new FileWriter(args[1]);
+			BufferedWriter output = new BufferedWriter(fstream);
+            while ((line = br.readLine()) != null) {
+			    String[] values = line.split(",");
+			    String  fileName = values[0];
+			    String  tf_string = values[1];
+			    Double tf = Double.parseDouble(tf_string);
+			    Double result = tf * multiplier;
+			    output.write(args[0] + " , " + fileName + " : " + result + "\n");
+			    
+			}
+            output.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ TF-IDF/src/term_freq.java
@@ -0,0 +1,64 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.io.*;
+
+
+public class term_freq {
+    //Method to invoke : java term_freq <input_filename> <output_file_name>
+	public static void main(String[] args) {
+
+		BufferedReader br=null;
+		try {
+			br = new BufferedReader(new FileReader(args[0]));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			System.out.println("The specified file " + args[0] + "was not found");
+			e1.printStackTrace();
+		}
+        String line = "";
+        HashMap<String, Integer> wordmap = new HashMap<String, Integer>();
+		Integer totalCount = 0;
+        try {
+			while ((line = br.readLine()) != null) {
+			   
+				String[] tokens = line.toLowerCase().split("[^a-z\n]");
+				
+				for (String word : tokens) {
+					Integer count = 1;
+					if (word.trim().equals("")) {
+						continue;
+					}
+					totalCount++;
+					if (wordmap.containsKey(word)) {
+                        count += (Integer)wordmap.get(word);
+                        wordmap.remove(word);
+					}
+					wordmap.put(word, count);
+				}
+			}
+			
+			Iterator<String> iterator = wordmap.keySet().iterator();
+			FileWriter fstream = new FileWriter(args[1]);
+			BufferedWriter output = new BufferedWriter(fstream);
+			
+			while (iterator.hasNext()) {
+				String key = (String) iterator.next();
+				Integer count = wordmap.get(key);
+				//output.write(key + ":"+ count.doubleValue()/totalCount.doubleValue() + "\n");
+				output.write(key + ":"+ args[0] + " , " + count.doubleValue()/totalCount.doubleValue() + "\n");
+			}
+			output.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			br.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
